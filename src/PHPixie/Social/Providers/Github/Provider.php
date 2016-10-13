a38a893662328924d7930687bf4504a9bc8b232d
rez1dent3 <maksim.babichev95@gmail.com>
2016-10-13 10:04:06 +0300

    add github patch instagram

---
+++ src/PHPixie/Social/Providers/Github/Provider.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace PHPixie\Social\Providers\GitHub;
+
+class Provider extends \PHPixie\Social\OAuth\OAuth2\Provider
+{
+    /**
+     * @var string The user endpoint for this provider. It will be passed to the endpointUrl method.
+     */
+    protected $loginDataEndpoint = 'user';
+
+    protected function endpointUrl($endpoint)
+    {
+        return 'https://api.github.com/' . $endpoint;
+    }
+
+    /**
+     * @param       $callbackUrl     The URL in the application where users will be sent after the authentication.
+     *                               The one specified in the OAuth Application settings page on GitHub, must be the
+     *                               same as the one in the loginCallback property in the app's routeResolver.php
+     *                               config file.
+     *
+     * @param array $additionalScope Additional scopes that will be requested to the user
+     *
+     * @return mixed The url for github.com to authorize this application
+     */
+    public function loginUrl($callbackUrl, $additionalScope = array())
+    {
+        $scope = array_merge(
+            $this->configData->get('scope', array()),
+            $additionalScope
+        );
+
+        return $this->buildLoginUrl(
+            'https://github.com/login/oauth/authorize',
+            $callbackUrl,
+            array('scope' => implode(',', $scope))
+        );
+    }
+
+    /**
+     * The method is called to obtain the access token, useful for the requests to the API.
+     * The Accept header is specified, because GitHub returns a string by default. JSON is needed.
+     *
+     * @param $callbackData   Data that are returned from the provider
+     * @param $baseParameters Array. Contains: client_id, client_secret, redirect_uri and code. Those parameters are
+     *                        necessaries for obtaining the token.
+     *
+     * @return mixed The token response from the API, based on the parameters of baseParameters param.
+     */
+    protected function getTokenResponse($callbackData, $baseParameters)
+    {
+        return $this->http()->call(
+            'POST',
+            'https://github.com/login/oauth/access_token',
+            $baseParameters,
+            null,
+            array('Accept' => 'application/json')
+        );
+    }
+
+    public function handleCallback($callbackUrl, $callbackData)
+    {
+        if(!isset($callbackData['code'])) {
+            return null;
+        }
+
+        $baseParameters = array(
+            'client_id'     => $this->configData->getRequired('appId'),
+            'client_secret' => $this->configData->getRequired('appSecret'),
+            'redirect_uri'  => $callbackUrl,
+            'code'          => $callbackData['code']
+        );
+
+        $tokenData = $this->getTokenResponse($callbackData, $baseParameters);
+        $tokenData = $this->decodeApiResponse($tokenData);
+
+        $loginData = $this->apiCall(
+            $tokenData->access_token,
+            'GET',
+            $this->loginDataEndpoint
+        );
+
+        $loginData = $this->normalizeLoginData($loginData, $tokenData);
+
+        $expiresIn    = $this->configData->get('expiresIn', 2592000);
+
+        $token = $this->token(
+            $this->getUserId($loginData),
+            $tokenData->access_token,
+            $expiresIn
+        );
+
+        return $this->user($token, $loginData);
+    }
+
+    /**
+     * Makes a call to the GitHub API.
+     * It's overriden because GitHub needs an User-Agent header. The value of the User-Agent header is taken from the
+     * social config file.
+     * The version is needed to make a call to a specific API version. It's also added to the social.php file.
+     *
+     * @return mixed The json from the API
+     * @see https://developer.github.com/v3/#user-agent-required
+     */
+    protected function apiCall($accessToken, $method, $endpoint, $query = array(), $data = null)
+    {
+        /**
+         * @url https://github.com/OAI/OpenAPI-Specification/blob/master/fixtures/v2.0/json/resources/multipleMimeTypes.json#L23
+         */
+        $format    = $this->configData->get('format', '+json');
+        $userAgent = $this->configData->get('userAgent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0');
+        $version   = $this->configData->get('version', 3);
+
+        $url                   = $this->endpointUrl($endpoint);
+        $query['access_token'] = $accessToken;
+        $response              = $this->http()->call($method, $url, $query, $data,
+            array(
+                'User-Agent' => $userAgent,
+                'Accept'     => 'application/vnd.github.' . $version . $format,
+            )
+        );
+
+        return $this->decodeApiResponse($response);
+    }
+
+    protected function buildToken($tokenData, $loginData)
+    {
+        $expiresIn    = $this->configData->get('expiresIn', 2592000);
+
+        return $this->token(
+            $loginData->id,
+            $tokenData->access_token,
+            $expiresIn
+        );
+    }
+
+    public function type()
+    {
+        return 'github';
+    }
+}
